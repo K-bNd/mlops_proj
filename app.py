2bd4ee9ca5998f23856fc01df84b8408c75e07a0
KB <kevin-brian.n-diaye@epita.fr>
2023-11-06 00:18:57 +0100

    feat(fastapi): fastapi transition done

--- app.py
+++ app.py
@@ -1,77 +1,77 @@
 import os
-from flask import Flask, request, send_from_directory, redirect, flash
-from app_utils import allowed_extension, download_file
+
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.background import BackgroundTask
+from starlette.middleware.sessions import SessionMiddleware
+from pydantic import BaseModel
+
+from app_utils import Settings, allowed_extension, download_file, flash
 from transcript import Transcript
 
-app = Flask(__name__, instance_relative_config=True)
+app = FastAPI()
 
-DEEPL_KEY = open("DEEPL_KEY.txt", "rb").readline().decode().rstrip()
-UPLOAD_FOLDER = os.path.join(app.root_path, 'upload_files')
+settings = Settings()
+app.add_middleware(SessionMiddleware, secret_key=settings.secret_key)
 
-obj = Transcript(DEEPL_KEY)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SECRET_KEY'] = 'dev'
+app.mount("/upload_files", StaticFiles(directory="./upload_files"),
+          name="upload_files")
 
-@app.route('/')
+obj = Transcript(settings.deepl_key)
+
+
+class Param(BaseModel):
+    """Define param class"""
+    file: str
+
+
+@app.get('/')
 def root():
     """Show home page."""
     return "Welcome page."
 
-@app.route('/transcript', methods=['GET'])
-def get_transcript():
+
+@app.get('/transcript')
+def get_transcript(request: Request, param: Param):
     """Get transcript"""
-    params = request.get_json()
-    if "file" not in list(params.keys()):
-        flash('No file given')
-        return redirect(request.url)
+    if not allowed_extension(param.file):
+        print(param.file)
+        err = BackgroundTask(
+            flash, request, "Unallowed extension for audio file")
+        return RedirectResponse(url="/", status_code=302, background=err)
 
-    if not allowed_extension(params["file"]):
-        print(request.get_json()["file"])
-        flash('Unallowed extension for audio file')
-        return redirect(request.url)
+    filename = os.path.join(settings.upload_folder,
+                            param.file.rsplit('/', maxsplit=1)[-1])
 
-    url = request.get_json()["file"]
-    filename = UPLOAD_FOLDER + url.rsplit('/', maxsplit=1)[-1]
-
-    download_file(url, filename)
+    download_file(param.file, filename)
     res = obj.get_transcript(filename)["text"]
     os.remove(filename)
     return res
 
-@app.route('/subtitles', methods=['GET'])
-def write_subtitles():
+
+@app.get('/subtitles', response_class=FileResponse)
+def write_subtitles(request: Request, param: Param):
     """Write subtitles for file"""
-    params = request.get_json()
-    if "file" not in list(params.keys()):
-        flash('No file given')
-        return redirect(request.url)
+    if not allowed_extension(param.file):
+        print(param.file)
+        err = BackgroundTask(
+            flash, request, "Unallowed extension for audio file")
+        return RedirectResponse(url="/", status_code=302, background=err)
 
-    if not allowed_extension(params["file"]):
-        print(params["file"])
-        flash('Unallowed extension for audio file')
-        return redirect(request.url)
-
-    url = params["file"]
-    filename = os.path.join(UPLOAD_FOLDER, url.rsplit('/', maxsplit=1)[-1])
+    filename = os.path.join(settings.upload_folder,
+                            param.file.rsplit('/', maxsplit=1)[-1])
     print(filename)
     subtitles_src = ".".join(filename.split('.')[0:-1]) + "-subtitles.vtt"
 
-    download_file(url, filename)
+    download_file(param.file, filename)
 
     obj.write_subtitles(
         filename, subtitles_src)
     os.remove(filename)
-    
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                            subtitles_src.rsplit('/', maxsplit=1)[-1], as_attachment=True)
-
-@app.route('/uploads/<path:filename>')
-def expose_file(filename):
-    """Make file available for users."""
-    print(app.config["UPLOAD_FOLDER"])
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                                filename, as_attachment=True)
+    print(subtitles_src)
+    return subtitles_src
 
 if __name__ == "__main__":
     print("test")
-    app.run(host ='0.0.0.0', port = 5000, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- app_utils.py
+++ app_utils.py
@@ -1,14 +1,41 @@
 from urllib.request import urlopen
 from shutil import copyfileobj
+from pydantic_settings import BaseSettings
+from fastapi import Request
 
 ALLOWED_EXTENSIONS = {'mp3', 'm4a', 'mp4'}
 
-def allowed_extension(filename):
+def allowed_extension(filename: str) -> bool:
     """Check if the extension from the uploaded file is valid."""
     return '.' in filename and \
            filename.rsplit('.', 1)[-1].lower() in ALLOWED_EXTENSIONS
 
-def download_file(url, filename) -> None:
+def download_file(url: str, filename: str) -> None:
     """Downloads a file from url into a file."""
     with urlopen(url) as in_stream, open(filename, 'wb+') as out_file:
             copyfileobj(in_stream, out_file)
+
+class Settings(BaseSettings):
+    """
+    Settings for the FastAPI app. Override with environment variables or .env file.
+
+    Override example:
+    `export FASTR_SECRET_KEY=secret`
+    """
+    secret_key: str = "dev"
+    deepl_key: str = open("DEEPL_KEY.txt", "rb").readline().decode().rstrip()
+    upload_folder: str = "./upload_files"
+
+def flash(request: Request, error: str) -> None:
+    """
+    Recreate the flash function from Flask. Store error messages in the "flashes" key
+    of the session so that all flashed messages can be displayed in a view.
+
+    Parameters
+    ----------
+    request
+        a fastapi request object
+    error
+        the error message to flash
+    """
+    request.session["flashes"] = request.session.get("flashes", []) + [error]
