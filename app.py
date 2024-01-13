a7768f0f12364ba1ae48ea32e064588d723e20c0
KB <kevin-brian.n-diaye@epita.fr>
2024-01-13 02:58:17 +0100

    feat(app): putting guardrails around 500

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
+from urllib.error import URLError
 
-from fastapi import FastAPI, Request, File, UploadFile
-from fastapi.responses import RedirectResponse, FileResponse
+from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from starlette.background import BackgroundTask
 from starlette.middleware.sessions import SessionMiddleware
 from pydantic import BaseModel
 
-from app_utils import Settings, allowed_extension, download_file, download_file_obj, flash
+from app_utils import Settings, allowed_extension, download_file
 from transcript import Transcript
 
 app = FastAPI()
@@ -15,44 +15,82 @@ app = FastAPI()
 settings = Settings()
 app.add_middleware(SessionMiddleware, secret_key=settings.secret_key)
 
-app.mount("/upload_files", StaticFiles(directory="./upload_files"),
-          name="upload_files")
-app.mount("/static", StaticFiles(directory="./static"),
-          name="static")
+app.mount("/upload_files", StaticFiles(directory="./upload_files"), name="upload_files")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 obj = Transcript(settings.deepl_key)
 
+
 class Param(BaseModel):
     """Define param class"""
+
     file: str
 
 
-@app.get('/')
+def remove_file(path: str) -> None:
+    """Remove file from disk"""
+    os.remove(path)
+
+
+@app.get("/")
 def root() -> FileResponse:
     """Show home page."""
     return FileResponse(path="./static/index.html", media_type="text/html")
 
-@app.get('/transcript')
-def get_transcript_obj(request: Request, file: UploadFile):
+
+@app.get("/transcript")
+def get_transcript(param: Param, background_tasks: BackgroundTasks):
     """Get transcript"""
-    if not allowed_extension(file.filename):
-        err = BackgroundTask(
-            flash, request, "Unallowed extension for audio file")
-        return RedirectResponse(url="/", status_code=302, background=err)
+    if not allowed_extension(param.file):
+        raise HTTPException(
+            status_code=422, detail="Unallowed extension for audio file"
+        )
+    filename = os.path.join(
+        settings.upload_folder, param.file.rsplit("/", maxsplit=1)[-1]
+    )
 
-    return obj.get_transcript(file.file)["text"]
+    try:
+        path = download_file(param.file, filename)
+    except (URLError, ValueError) as err:
+        raise HTTPException(
+            status_code=403,
+            detail="Server does not have access to the content",
+        ) from err
+    except FileNotFoundError as err:
+        raise HTTPException(
+            status_code=502, detail="Server could not write file to disk"
+        ) from err
 
-@app.get('/subtitles', response_class=FileResponse)
-def write_subtitles_obj(request: Request, file: UploadFile):
+    background_tasks.add_task(remove_file, path)
+    return obj.get_transcript(path)["text"]
+
+
+@app.get("/subtitles", response_class=FileResponse)
+def write_subtitles(param: Param, background_tasks: BackgroundTasks):
     """Write subtitles for file"""
-    if not allowed_extension(file.filename):
-        err = BackgroundTask(
-            flash, request, "Unallowed extension for audio file")
-        return RedirectResponse(url="/", status_code=302, background=err)
+    if not allowed_extension(param.file):
+        raise HTTPException(
+            status_code=422, detail="Unallowed extension for audio file"
+        )
 
-    filename = os.path.join(settings.upload_folder, file.filename)
-    subtitles_src = ".".join(filename.split('.')[0:-1]) + "-subtitles.vtt"
+    filename = os.path.join(
+        settings.upload_folder, param.file.rsplit("/", maxsplit=1)[-1]
+    )
+    subtitles_src = ".".join(filename.split(".")[0:-1]) + "-subtitles.vtt"
 
-    obj.write_subtitles(
-        file.file, subtitles_src)
+    try:
+        path = download_file(param.file, filename)
+    except (URLError, ValueError) as err:
+        raise HTTPException(
+            status_code=403,
+            detail="Server does not have access to the content",
+        ) from err
+    except FileNotFoundError as err:
+        raise HTTPException(
+            status_code=502, detail="Server could not write file to disk"
+        ) from err
+
+    obj.write_subtitles(path, subtitles_src)
+    background_tasks.add_task(remove_file, path)
+    background_tasks.add_task(remove_file, subtitles_src)
     return subtitles_src
--- app_utils.py
+++ app_utils.py
@@ -2,26 +2,29 @@ import os
 from urllib.request import urlopen
 from shutil import copyfileobj
 from pydantic_settings import BaseSettings
-from fastapi import Request, UploadFile
 
-ALLOWED_EXTENSIONS = {'mp3', 'm4a', 'mp4', 'mov'}
+ALLOWED_EXTENSIONS = {"mp3", "m4a", "mp4", "mov"}
+
 
 def allowed_extension(filename: str) -> bool:
     """Check if the extension from the uploaded file is valid."""
-    print(filename)
-    return '.' in filename and \
-           filename.rsplit('.', 1)[-1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[-1].lower() in ALLOWED_EXTENSIONS
 
-def download_file(url: str, filename: str) -> None:
+
+def download_file(url: str, filename: str) -> str:
     """Downloads a file from url into a file."""
-    with urlopen(url) as in_stream, open(filename, 'wb+') as out_file:
-            copyfileobj(in_stream, out_file)
+    r = urlopen(url)
+    if r.url != url:
+        raise ValueError("Download error, failed to download content")
 
-def download_file_obj(file: UploadFile) -> None:
-    """Downloads a file from UploadFile into a file."""
-    settings = Settings()
-    with open(os.path.join(settings.upload_folder, file.filename), 'wb') as f:
-            copyfileobj(file.file, f)
+    with r as in_stream, open(filename, "wb+") as out_file:
+        copyfileobj(in_stream, out_file)
+
+    if not os.path.exists(filename):
+        raise FileNotFoundError("File could be not created")
+
+    return os.path.abspath(filename)
+
 
 class Settings(BaseSettings):
     """
@@ -30,20 +33,7 @@ class Settings(BaseSettings):
     Override example:
     `export FASTR_SECRET_KEY=secret`
     """
+
     secret_key: str = "dev"
     deepl_key: str = os.environ.get("DEEPL_KEY", "")
     upload_folder: str = "./upload_files"
-
-def flash(request: Request, error: str) -> None:
-    """
-    Recreate the flash function from Flask. Store error messages in the "flashes" key
-    of the session so that all flashed messages can be displayed in a view.
-
-    Parameters
-    ----------
-    request
-        a fastapi request object
-    error
-        the error message to flash
-    """
-    request.session["flashes"] = request.session.get("flashes", []) + [error]
